67f2b66d014e1a9a4c5a90e0bf0ec0edbbd294f2
unknown <heojun8500@gmail.com>
2025-09-30 19:31:39 +0900

    mediapipe 버전 충돌 방지용 코드 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // 일부 Node.js 내장 모듈 참조 막기
+    config.resolve.fallback = {
+      fs: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
