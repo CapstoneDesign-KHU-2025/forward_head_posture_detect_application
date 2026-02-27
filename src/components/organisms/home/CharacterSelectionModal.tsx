93934bb8f40abcdf8caaeb38b7d5128e14520f2b
jimin <dnpsel2737@gmail.com>
2026-02-27 17:45:32 +0900

    refactor: move settings modals to home organisms layer (TNA-30)

--- src/components/organisms/home/CharacterSelectionModal.tsx
+++ src/components/organisms/home/CharacterSelectionModal.tsx
@@ -130,3 +130,4 @@ export default function CharacterSelectionModal({ isOpen, onClose }: CharacterSe
     </Modal>
   );
 }
+
--- src/components/organisms/home/SensitivitySettingsModal.tsx
+++ src/components/organisms/home/SensitivitySettingsModal.tsx
@@ -106,3 +106,4 @@ export default function SensitivitySettingsModal({ isOpen, onClose }: Sensitivit
     </Modal>
   );
 }
+
